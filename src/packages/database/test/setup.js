4afdd3831633e55e350fb569182f988557d481bd
Harald Schilly <harald.schilly@gmail.com>
2025-12-30 12:40:30 +0100

    database: continue server queries

--- src/packages/database/test/setup.js
+++ src/packages/database/test/setup.js
@@ -10,4 +10,4 @@ if (usePglite && !process.env.COCALC_PGLITE_DATA_DIR) {
 
 // see packages/database/pool/pool.ts for where this name is also hard coded:
 process.env.PGDATABASE = "smc_ephemeral_testing_database";
-
+process.env.PGTZ = "UTC";
