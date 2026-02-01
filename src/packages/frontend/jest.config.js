7134aa189f4ea00998055ba6902513aa409648d4
William Stein <wstein@sagemath.com>
2026-01-31 17:31:44 -0800

    fix(frontend): map users mock before generic

--- src/packages/frontend/jest.config.js
+++ src/packages/frontend/jest.config.js
@@ -13,14 +13,14 @@ module.exports = {
       "<rootDir>/test/mocks/codemirror-static.js",
     "^@cocalc/frontend/codemirror/init$":
       "<rootDir>/test/mocks/codemirror-init.js",
+    "^@cocalc/frontend/users$": "<rootDir>/test/mocks/frontend-users.js",
+    "^@cocalc/frontend/frame-editors/generic/chat$":
+      "<rootDir>/test/mocks/generic-chat.js",
     "^@cocalc/frontend/(.*)$": "<rootDir>/$1",
     "^p-limit$": "<rootDir>/test/mocks/p-limit.js",
     "^dropzone$": "<rootDir>/test/mocks/dropzone.js",
-    "^@cocalc/frontend/users$": "<rootDir>/test/mocks/frontend-users.js",
     "^\\.\\./users$": "<rootDir>/test/mocks/frontend-users.js",
     "^\\.\\./\\.\\./users$": "<rootDir>/test/mocks/frontend-users.js",
-    "^@cocalc/frontend/frame-editors/generic/chat$":
-      "<rootDir>/test/mocks/generic-chat.js",
     "^\\.\\./generic/chat$": "<rootDir>/test/mocks/generic-chat.js",
     "^@xterm/xterm$": "<rootDir>/test/mocks/xterm.js",
     "^@xterm/addon-fit$": "<rootDir>/test/mocks/xterm-addon.js",
