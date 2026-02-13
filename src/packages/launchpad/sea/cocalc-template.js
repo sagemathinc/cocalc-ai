1ece018d441401d3fb01a45156cca59900d57af8
William Stein <wstein@sagemath.com>
2026-02-13 15:25:21 -0800

    sea: normalize argv forwarding for embedded entrypoints

--- src/packages/launchpad/sea/cocalc-template.js
+++ src/packages/launchpad/sea/cocalc-template.js
@@ -91,10 +91,7 @@ if (path.basename(process.argv[1]) == "node") {
   }
 
   process.chdir(path.dirname(script));
-  const argv = process.argv.slice(1);
-  if (argv[0] === process.argv[0]) {
-    argv.shift();
-  }
+  const argv = process.argv.slice(2);
   process.argv = [process.execPath, script, ...argv];
   process.env.COCALC_BIN_PATH = path.join(destDir, `src/packages/${name}/bin/`);
 
