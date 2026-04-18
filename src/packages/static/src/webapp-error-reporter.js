6455909517a993cbba3fcc3d053d5bddc65837b4
William Stein <wstein@sagemath.com>
2026-04-18 12:19:31 -0700

    static/error-reporter: remove invasive browser monkeypatching
    Simplify the legacy webapp error reporter so it focuses on surfacing real
    uncaught browser errors instead of wrapping large parts of the runtime.
    
    The old implementation monkeypatched event listeners, timer callbacks,
    requestAnimationFrame, setImmediate, and console methods. That approach was
    high-risk and expensive, and it could amplify existing browser stalls by adding
    wrappers and stack generation around hot callback paths.
    
    Keep the useful low-level reporting paths: `window.onerror` and
    `unhandledrejection`. Remove the global callback wrapping and console/timer
    instrumentation so the reporter is less likely to distort runtime behavior while
    still reporting genuine uncaught failures.

--- src/packages/static/src/webapp-error-reporter.js
+++ src/packages/static/src/webapp-error-reporter.js
@@ -22,12 +22,6 @@
 let ENABLED;
 const already_reported = [];
 
-const FUNCTION_REGEX = /function\s*([\w\-$]+)?\s*\(/i;
-
-let ignoreOnError = 0;
-
-let shouldCatch = true;
-
 // set this to true, to enable the webapp error reporter for development
 const enable_for_testing = false;
 if (typeof BACKEND !== "undefined" && BACKEND) {
@@ -169,285 +163,43 @@ const sendError = async function (opts) {
 
 // neat trick to get a stacktrace when there is none
 var generateStacktrace = function () {
-  let stacktrace;
-  let generated = (stacktrace = null);
-  const MAX_FAKE_STACK_SIZE = 10;
-  const ANONYMOUS_FUNCTION_PLACEHOLDER = "[anonymous]";
-
   try {
     throw new Error("");
   } catch (exception) {
-    generated = "<generated>\n";
-    stacktrace = stacktraceFromException(exception);
+    return `<generated>\n${stacktraceFromException(exception) || ""}`;
   }
-
-  if (!stacktrace) {
-    generated = "<generated-ie>\n";
-    const functionStack = [];
-    try {
-      let curr = arguments.callee.caller.caller;
-      while (curr && functionStack.length < MAX_FAKE_STACK_SIZE) {
-        var fn;
-        if (FUNCTION_REGEX.test(curr.toString())) {
-          fn = RegExp.$1 != null ? RegExp.$1 : ANONYMOUS_FUNCTION_PLACEHOLDER;
-        } else {
-          fn = ANONYMOUS_FUNCTION_PLACEHOLDER;
-        }
-        functionStack.push(fn);
-        curr = curr.caller;
-      }
-    } catch (e) {}
-    //console.error(e)
-    stacktrace = functionStack.join("\n");
-  }
-  return generated + stacktrace;
+  return "<generated>\n";
 };
 
 var stacktraceFromException = (exception) =>
   exception.stack || exception.backtrace || exception.stacktrace;
 
-// Disable catching on IE < 10 as it destroys stack-traces from generateStackTrace()
-// OF COURSE, COCALC doesn't support any version of IE at all, so ...
-if (!window.atob) {
-  shouldCatch = false;
-}
-
-// Disable catching on browsers that support HTML5 ErrorEvents properly.
-// This lets debug on unhandled exceptions work.
-// TODO: enabling the block below distorts (at least) Chrome error messages.
-// Maybe Chrome's window.onerror doesn't work as assumed?
-// else if window.ErrorEvent
-//     try
-//         if new window.ErrorEvent("test").colno == 0
-//             shouldCatch = false
-//     catch e
-//         # No action needed
-
-// flag to ignore "onerror" when already wrapped in the event handler
-const ignoreNextOnError = function () {
-  ignoreOnError += 1;
-  return window.setTimeout(() => (ignoreOnError -= 1));
-};
-
-// this is the "brain" of all this
-const wrap = function (_super) {
-  try {
-    if (typeof _super !== "function") {
-      return _super;
-    }
-
-    if (!_super._wrapper) {
-      _super._wrapper = function () {
-        if (shouldCatch) {
-          try {
-            return _super.apply(this, arguments);
-          } catch (e) {
-            reportException(e, null, "error");
-            ignoreNextOnError();
-            throw e;
-          }
-        } else {
-          return _super.apply(this, arguments);
-        }
-      };
-
-      _super._wrapper._wrapper = _super._wrapper;
-    }
-
-    return _super._wrapper;
-  } catch (error) {
-    const e = error;
-    return _super;
-  }
-};
-
-// replaces an attribute of an object by a function that has it as an argument
-const polyFill = function (obj, name, makeReplacement) {
-  const original = obj[name];
-  const replacement = makeReplacement(original);
-  return (obj[name] = replacement);
-};
-
-// wrap all prototype objects that have event handlers
-// first one is for chrome, the first three for FF, the rest for IE, Safari, etc.
 if (ENABLED) {
-  "EventTarget Window Node ApplicationCache AudioTrackList ChannelMergerNode CryptoOperation EventSource FileReader HTMLUnknownElement IDBDatabase IDBRequest IDBTransaction KeyOperation MediaController MessagePort ModalWindow Notification SVGElementInstance Screen TextTrack TextTrackCue TextTrackList WebSocket WebSocketWorker Worker XMLHttpRequest XMLHttpRequestEventTarget XMLHttpRequestUpload".replace(
-    /\w+/g,
-    function (global) {
-      const prototype = window[global]?.prototype;
-      if (prototype?.hasOwnProperty?.("addEventListener")) {
-        polyFill(
-          prototype,
-          "addEventListener",
-          (_super) =>
-            function (e, f, capture, secure) {
-              try {
-                if (f && f.handleEvent) {
-                  f.handleEvent = wrap(f.handleEvent);
-                }
-              } catch (err) {}
-              //console.log(err)
-              return _super.call(this, e, wrap(f), capture, secure);
-            },
-        );
-
-        return polyFill(
-          prototype,
-          "removeEventListener",
-          (_super) =>
-            function (e, f, capture, secure) {
-              _super.call(this, e, f, capture, secure);
-              return _super.call(this, e, wrap(f), capture, secure);
-            },
-        );
-      }
-    },
-  );
-}
-
-if (ENABLED) {
-  polyFill(
-    window,
-    "onerror",
-    (_super) =>
-      function (message, url, lineNo, charNo, exception) {
-        // IE 6+ support.
-        if (!charNo && window.event) {
-          charNo = window.event.errorCharacter;
-        }
-
-        //if DEBUG
-        //    console.log("intercepted window.onerror", message, url, lineNo, charNo, exception)
-
-        if (ignoreOnError === 0) {
-          const name = exception?.name || "window.onerror";
-          const stacktrace =
-            (exception && stacktraceFromException(exception)) ||
-            generateStacktrace();
-          sendError({
-            name,
-            message,
-            file: url,
-            path: window.location.href,
-            lineNumber: lineNo,
-            columnNumber: charNo,
-            stacktrace,
-            severity: "error",
-          });
-        }
-
-        // Fire the existing `window.onerror` handler, if one exists
-        if (_super) {
-          return _super(message, url, lineNo, charNo, exception);
-        }
-      },
-  );
-}
-
-// timing functions
-
-// I was hitting hangs and this is using a lot of time in the profiler during this...
-
-// const hijackTimeFunc = (_super) =>
-//   function (f, t) {
-//     if (typeof f === "function") {
-//       f = wrap(f);
-//       const args = Array.prototype.slice.call(arguments, 2);
-//       return _super(function () {
-//         return f.apply(this, args);
-//       }, t);
-//     } else {
-//       return _super(f, t);
-//     }
-//   };
-
-// if (ENABLED) {
-//   polyFill(window, "setTimeout", hijackTimeFunc);
-//   polyFill(window, "setInterval", hijackTimeFunc);
-// }
-
-if (ENABLED && window.requestAnimationFrame) {
-  polyFill(
-    window,
-    "requestAnimationFrame",
-    (_super) => (callback) => _super(wrap(callback)),
-  );
-}
-
-if (ENABLED && window.setImmediate) {
-  polyFill(
-    window,
-    "setImmediate",
-    (_super) =>
-      function () {
-        const args = Array.prototype.slice.call(arguments);
-        args[0] = wrap(args[0]);
-        return _super.apply(this, args);
-      },
-  );
-}
-
-// console terminal
-
-function argsToJson(args) {
-  let v = [];
-  try {
-    const misc = require("@cocalc/util/misc");
-    for (const arg of args) {
-      try {
-        const s = JSON.stringify(arg);
-        v.push(s.length > 1000 ? misc.trunc_middle(s) : JSON.parse(s));
-      } catch (_) {
-        v.push("(non-jsonable-arg)");
-      }
-      if (v.length > 10) {
-        v.push("(skipping JSON of some args)");
-        break;
-      }
+  const previousOnError = window.onerror;
+  window.onerror = function (message, url, lineNo, charNo, exception) {
+    // IE 6+ support.
+    if (!charNo && window.event) {
+      charNo = window.event.errorCharacter;
     }
-  } catch (_) {
-    // must be robust.
-    v.push("(unable to JSON some args)");
-  }
-  return JSON.stringify(v);
-}
 
-const sendLogLine = (severity, args) => {
-  let message;
-  if (typeof args === "object") {
-    message = argsToJson(args);
-  } else {
-    message = Array.prototype.slice.call(args).join(", ");
-  }
-  sendError({
-    name: "Console Output",
-    message,
-    file: "",
-    path: window.location.href,
-    lineNumber: -1,
-    columnNumber: -1,
-    stacktrace: generateStacktrace(),
-    severity,
-  });
-};
+    const name = exception?.name || "window.onerror";
+    const stacktrace =
+      (exception && stacktraceFromException(exception)) || generateStacktrace();
+    sendError({
+      name,
+      message,
+      file: url,
+      path: window.location.href,
+      lineNumber: lineNo,
+      columnNumber: charNo,
+      stacktrace,
+      severity: "error",
+    });
 
-const wrapFunction = function (object, property, newFunction) {
-  const oldFunction = object[property];
-  return (object[property] = function () {
-    newFunction.apply(this, arguments);
-    if (typeof oldFunction === "function") {
-      return oldFunction.apply(this, arguments);
+    if (typeof previousOnError === "function") {
+      return previousOnError(message, url, lineNo, charNo, exception);
     }
-  });
-};
-
-if (ENABLED && window.console != null) {
-  wrapFunction(console, "warn", function () {
-    return sendLogLine("warn", arguments);
-  });
-  wrapFunction(console, "error", function () {
-    return sendLogLine("error", arguments);
-  });
+  };
 }
 
 if (ENABLED) {
@@ -478,17 +230,13 @@ if (DEBUG) {
   }
   window.cc.webapp_error_reporter = {
     shouldCatch() {
-      return shouldCatch;
-    },
-    ignoreOnError() {
-      return ignoreOnError;
+      return false;
     },
     already_reported() {
       return already_reported;
     },
     stacktraceFromException,
     generateStacktrace,
-    sendLogLine,
     reportException,
     is_enabled() {
       return ENABLED;
